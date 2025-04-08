0a70c5f04e547511d34351630011eebbd064787b
BulmasArmpitSweat <151138740+BulmasArmpitSweat@users.noreply.github.com>
2025-04-08 14:04:25 +1000

    Update lib.rs
    Nvm it wasn't the final version. This should be it here,

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,8 @@
 extern crate inline_colorization;
 extern crate errno;
 extern crate tinyvec_string;
+extern crate print_no_std;
+extern crate exit_no_std;
 
 use inline_colorization::*;
 use core::fmt::Write;
@@ -18,7 +20,10 @@ fn _die(code: i32, colour: &str, args: core::fmt::Arguments) -> ! {
     errno::set_errno(errno::Errno(code));
     let mut output: tinyvec_string::tinystring::TinyString<[u8; 32]> = tinyvec_string::tinystring::TinyString::new();
     output.write_fmt(args).unwrap();
-    panic!(
+    // A1-Triard, my saviour: https://crates.io/users/A1-Triard
+    // Thanks you for the so useful print_no_std and exit_no_std
+    // I'm gonna suck your di-
+    print_no_std::println!(
         "{style_bold}{}[ ERROR ({}:{}) ]: {}: {}, {}{style_reset}{color_reset}",
         colour,
         file!(),
@@ -27,6 +32,7 @@ fn _die(code: i32, colour: &str, args: core::fmt::Arguments) -> ! {
         errno::errno(),
         output
     );
+    exit_no_std::exit(u8::MAX);
 }
 
 /// Abort program with fancy output
